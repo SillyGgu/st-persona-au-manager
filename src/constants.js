341a5f474d450d0d4d7a599b7a91c98e353f33e1
실꾸 <rkdud4503@gmail.com>
2025-12-28 20:03:01 +0900

    Delete src directory

--- src/constants.js
+++
@@ -1,6 +0,0 @@
-export const MODULE_NAME = "st-persona-au-manager";
-export const STORAGE_KEYS = {
-    PERSONA_DATA: 'paum_data_v1', // AU 리스트 저장
-    BACKUP: 'paum_backup_v1'      // 전체 백업용
-};
-export const BUTTON_ID = 'paum_open_btn';
